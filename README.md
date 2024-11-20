1681dadf77b341a0ea304aac12721c869f4deb49
Neeraj Dalal <admin@nrjdalal.com>
2024-11-20 06:10:01 +0530

    feat: v1.1.0-latest

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
-# ai-terminal
+# AI X Terminal
 
-To install dependencies:
+**AI X Terminal** is a command-line tool that leverages OpenAI's API to enhance your terminal experience with AI capabilities. With options to append file contents or entire workspaces to requests, AI Terminal facilitates seamless integration with your current projects.
 
-```bash
-bun install
-```
-
-To run:
+## Installation
 
 ```bash
-bun run src/index.ts
+npm install -g ai-x-terminal
 ```
 
-This project was created using `bun init` in bun v1.1.34. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+OR
+
+```bash
+bun add ai-x-terminal
+```
+
+## Features
+
+- **Command Line Arguments**: Use the tool with various options to customize its behavior.
+
+  - `-f, --file <file>`: Append specified file content to the AI prompt.
+  - `-w, --workspace`: Append contents of all files in the current directory to the prompt.
+
+- **OpenAI Integration**:
+  - Upon first run, you will be prompted to input your OpenAI API key, which will be securely stored for future use.
+
+## Note
+
+Feel free to contribute or raise issues if you encounter any!
+
+---
+
+**Disclaimer**: Make sure to keep your API key confidential and never share it publicly.
--- src/index.ts
+++ src/index.ts
@@ -14,105 +14,120 @@ process.on('SIGTERM', () => process.exit(0))
 async function main() {
   program
     .name('x')
-    .option('-f, --file <file>', 'append file contents to prompt')
+    .option('-f, --file <file>', 'append file content to prompt')
+    .option('-w, --workspace', 'append all files in directory to prompt')
     .arguments('<args...>')
-    .action(async (args: string[], opts: { file: string }) => {
-      const configFile = path.join(os.homedir(), '/.config/.ai-terminal')
+    .action(
+      async (args: string[], opts: { file: string; workspace: boolean }) => {
+        const configFile = path.join(os.homedir(), '/.config/.ai-terminal')
 
-      if (!fs.existsSync(configFile)) {
-        console.error('config file not found')
+        if (!fs.existsSync(configFile)) {
+          console.error('config file not found')
 
-        const api = await password({
-          message: 'API Key',
-          mask: '*',
-        })
+          const api = await password({
+            message: 'API Key',
+            mask: '*',
+          })
 
-        const testApiKey = async (apiKey: string) => {
-          try {
-            const response = await fetch('https://api.openai.com/v1/models', {
-              method: 'GET',
-              headers: {
-                Authorization: `Bearer ${apiKey}`,
-              },
-            })
-            const res = (await response.json()) as any
+          const testApiKey = async (apiKey: string) => {
+            try {
+              const response = await fetch('https://api.openai.com/v1/models', {
+                method: 'GET',
+                headers: {
+                  Authorization: `Bearer ${apiKey}`,
+                },
+              })
+              const res = (await response.json()) as any
 
-            if (response.ok) {
-              console.log(
-                'API key is valid. Models available:',
-                res.data.map((model: any) => model.id)
-              )
-              return true
-            } else {
-              console.error('Error:', res.error.message)
+              if (response.ok) {
+                console.log(
+                  'API key is valid. Models available:',
+                  res.data.map((model: any) => model.id)
+                )
+                return true
+              } else {
+                console.error('Error:', res.error.message)
+                return false
+              }
+            } catch (error: any) {
+              console.error('Error connecting to OpenAI API:', error.message)
               return false
             }
-          } catch (error: any) {
-            console.error('Error connecting to OpenAI API:', error.message)
-            return false
+          }
+
+          const isValidApiKey = await testApiKey(api)
+
+          if (isValidApiKey) {
+            fs.writeFileSync(configFile, api)
+            console.log('API Key saved to config file.')
+          } else {
+            console.error('Invalid API key. Exiting...')
+            return
           }
         }
 
-        const isValidApiKey = await testApiKey(api)
+        const apiKey = fs.readFileSync(configFile, 'utf-8').trim()
 
-        if (isValidApiKey) {
-          fs.writeFileSync(configFile, api)
-          console.log('API Key saved to config file.')
-        } else {
-          console.error('Invalid API key. Exiting...')
-          return
-        }
-      }
-
-      const apiKey = fs.readFileSync(configFile, 'utf-8').trim()
-
-      const openai = new OpenAI({
-        apiKey,
-      })
-
-      const streamCompletion = async (prompt: string) => {
-        const stream = await openai.chat.completions.create({
-          model: 'gpt-4o',
-          messages: [
-            {
-              role: 'system',
-              content: 'You are a helpful assistant.',
-            },
-            { role: 'user', content: prompt },
-          ],
-          stream: true,
+        const openai = new OpenAI({
+          apiKey,
         })
 
-        for await (const part of stream) {
-          if (part.choices && part.choices[0]?.delta?.content) {
-            process.stdout.write(part.choices[0].delta.content)
+        const streamCompletion = async (prompt: string) => {
+          const stream = await openai.chat.completions.create({
+            model: 'gpt-4o',
+            messages: [
+              {
+                role: 'system',
+                content: 'You are a helpful assistant.',
+              },
+              { role: 'user', content: prompt },
+            ],
+            stream: true,
+          })
+
+          for await (const part of stream) {
+            if (part.choices && part.choices[0]?.delta?.content) {
+              process.stdout.write(part.choices[0].delta.content)
+            }
           }
         }
-      }
 
-      let prompt = args.join(' ')
+        let prompt = args.join(' ')
 
-      if (opts.file) {
-        let file = opts.file
+        if (opts.file) {
+          const searchFile = opts.file
 
-        const files = await globby('**/*', {
-          gitignore: true,
-        })
+          const files = await globby('**/*', {
+            gitignore: true,
+          })
 
-        const filePath = files.find((f) => f.includes(file))
+          const file = files.find((f) => f.includes(searchFile))
 
-        if (filePath) {
-          const fileContents = fs.readFileSync(filePath, 'utf-8')
+          if (file) {
+            const fileContents = fs.readFileSync(file, 'utf-8').trim() + '\n---'
 
-          prompt += `\n--- ${filePath}\n${fileContents}\n---`
-        } else {
-          console.error(`File not found: ${file}`)
-          return
+            prompt += `\n\n--- ${file}\n${fileContents}`
+          } else {
+            console.error(`File not found: ${file}`)
+            return
+          }
         }
-      }
 
-      await streamCompletion(prompt)
-    })
+        if (opts.workspace) {
+          const files = await globby('**/*', {
+            gitignore: true,
+          })
+
+          for (const file of files) {
+            const fileContents = fs.readFileSync(file, 'utf-8').trim() + '\n---'
+
+            prompt += `\n\n--- ${file}\n${fileContents}`
+          }
+        }
+
+        await streamCompletion(prompt)
+      }
+    )
 
   program.parse()
 }
