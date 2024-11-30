f85ed2e949681b38548477f7e47676663a3d8666
Neeraj Dalal <admin@nrjdalal.com>
2024-11-30 11:04:14 +0530

    feat: saving awesome ref state

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -1,5 +1,5 @@
-import chalk from '../utils/chalk.js'
 import { OpenAI } from 'openai'
+import chalk from '../utils/chalk.js'
 
 export async function streamCompletion(
   openai: OpenAI,
@@ -18,9 +18,45 @@ export async function streamCompletion(
     stream: true,
   })
 
+  let chunker = ''
+  let holdMode = false
+  let slidingWindow = []
+  let turnOffHoldMode = false
+  let codeblock = 0
+
   for await (const part of stream) {
     if (part.choices && part.choices[0]?.delta?.content) {
-      process.stdout.write(part.choices[0].delta.content)
+      const chunk = part.choices[0].delta.content
+
+      if (turnOffHoldMode) {
+        holdMode = false
+        turnOffHoldMode = false
+      }
+
+      if (holdMode || chunk.includes('`')) {
+        chunker += chunk
+        slidingWindow.push(chunk)
+        holdMode = true
+
+        if (slidingWindow.length === 3) {
+          process.stdout.write(chalk.green(chunker))
+          chunker = ''
+          turnOffHoldMode = true
+
+          if (slidingWindow.join('').includes('```')) {
+            if (codeblock === 1) {
+              codeblock--
+            }
+            codeblock++
+          }
+
+          slidingWindow = []
+        }
+      }
+
+      if (!holdMode) {
+        process.stdout.write(chunk)
+      }
     }
   }
 }
