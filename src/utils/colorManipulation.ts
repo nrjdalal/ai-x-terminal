8039f8ed37560ce5c7daaa2a0129ba967ef49f96
Neeraj Dalal <admin@nrjdalal.com>
2024-12-01 20:19:17 +0530

    feat: code update proposal added

--- src/utils/colorManipulation.ts
+++ src/utils/colorManipulation.ts
@@ -6,27 +6,25 @@ export const manipulateCode = (codeBlock: string, opts: any) => {
   if (!codeBlock.includes(`\`\`\``)) return codeBlock
 
   try {
-    const chunkParts = codeBlock.split(`\`\`\``)
-
     const code = {
-      language: chunkParts[1].split('\n')[0].split(' ')[0],
-      langpath: chunkParts[1].split('\n')[0].split(' ')[1] || '_noPathFound_',
-      content: chunkParts[1].split('\n').slice(1).join('\n'),
+      language:
+        codeBlock.split('```')[1].split('\n')[0].split(' ')[0] || 'plaintext',
+      langpath:
+        codeBlock.split('```')[1].split('\n')[0].split(' ')[1] || '__temp__',
+      content: codeBlock
+        .split('```')
+        .slice(1, -1)
+        .join('```')
+        .split('\n')
+        .slice(1)
+        .join('\n'),
     }
 
-    const precode =
-      chunkParts[0] +
-      `\`\`\`` +
-      code.language +
-      ' ' +
-      (code.langpath || '_noPathFound_') +
-      '\n'
-
+    const precode = codeBlock.slice(0, codeBlock.indexOf('```')) + '```'
     const lastOccurence = codeBlock.lastIndexOf('```')
     const postcode = `\`\`\`` + codeBlock.slice(lastOccurence + 3)
 
     if (opts.replace) {
-      console.log(chalk.green(`Writing to ${code.langpath}`))
       try {
         fs.writeFileSync(code.langpath, code.content.trim())
       } catch (err) {
